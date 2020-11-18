be7bc0fc27ce4ef6878d5959a3a2716279d4c87c
Harry Ayres <solar.granulation@gmail.com>
2020-11-18 16:59:16 +0000

    Building up the XML parser code, mostly guiding comments

--- main.gd
+++ main.gd
@@ -37,30 +37,51 @@ const ROOM = "room"
 const TITLE = "title"
 const DESCRIPTION = "description"
 
+func build_room(_stream:XMLParser):
+	var new_room:Room # Constructor takes title and description 0
+	var _title
+	var _description # Description 0
+	var _additional_descriptions = []
+	# Read the next line, controlling a while loop
+		# Check node type:
+		# If it's an element END:
+			# If it's /room:
+				#return
+			#otherwise:
+				#continue
+		# If it's an element: 
+		# Check element name:
+			# If title:
+				# Advance to next (text) element
+				# set _title
+			# If first description:
+				# Advance to next (text) element
+				# set _description
+			# If another description:
+				# Advance to next (text) element
+				# append to _additional_descriptions
+	######## This could be better. What if unexpected EOF? ########
+
 func _init():
 	var _elem:String
 	var _err = _roomstream.open(_roomfile)
 	
 	# Need to design the loops/functions
-	while _err == OK:
-		_err = _roomstream.read()
-		if _err != OK:
-			return _err
-		if _roomstream.get_node_type() == XMLParser.NODE_ELEMENT_END:
-			continue
-		_elem = _roomstream.get_node_name()
-		if _elem == TITLE:
-			print("Hi")
-			_err = _roomstream.read()
-			if _err != OK:
-				return _err
-			print(_roomstream.get_node_data())
-			print("Bye")
+	while _roomstream.read() == OK:
+		var _type = _roomstream.get_node_type() 
+		if _type == XMLParser.NODE_ELEMENT_END:
+			continue # If it's a closing tag, iterate
+		if _type == XMLParser.NODE_ELEMENT:
+			_elem = _roomstream.get_node_name()
+			if _elem == ROOM:
+				build_room(_roomstream)
+#			if _elem == TITLE:
+#				_err = _roomstream.read()
+#				if _err != OK:
+#					return _err
+#				if _roomstream.get_node_type() == XMLParser.NODE_TEXT:
+#					print(_roomstream.get_node_data())
+	return _err # _init()
 
-# _title:String
-# _descriptions = [] # An array, accessed using _current_state
-# _current_state:int
-# _ways = [] # An array of Way objects, linking rooms.
-# _inventory = [] # An array of Items
 
 # HERE ENDETH THE ROOMS
